2d9622b35f5d4cb8703ed12e109d84965019849c
tharindu <tharinduprf@gmail.com>
2016-11-20 09:50:51 +0530

    Documents added

--- test_script.py
+++ test_script.py
@@ -1,5 +1,7 @@
+from sklearn.externals import joblib
+from feature_extraction import *
 def test(ffile):
     clf = joblib.load('Model/model.pkl')
     feature_vector=extract_features(ffile,1)
     
-    return model.predict(feature_vector)
+    return clf.predict(feature_vector)
