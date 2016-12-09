bc163106462b04a629353da956ee465f8aaad43d
tharindu <tharinduprf@gmail.com>
2016-12-09 10:51:14 +0530

    Thesis

--- test_script.py
+++ test_script.py
@@ -1,7 +1,21 @@
 from sklearn.externals import joblib
 from feature_extraction import *
+from os import listdir
+
 def test(ffile):
     clf = joblib.load('Model/model.pkl')
     feature_vector=extract_features(ffile,1)
     
     return clf.predict(feature_vector)
+
+
+positive=listdir('Testing/Positive')
+negative=listdir('Testing/Negative')
+
+
+print "\n\nTesting positive set\n\n"
+for i in positive:
+    print test('Testing/Positive/'+i)
+print "\n\nTesting negative set\n\n"
+for i in negative:
+    print test('Testing/Negative/'+i)
